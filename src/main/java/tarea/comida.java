3bf82291b34eecd18192bceee0fb2b4677e33097
Moreno27 <alejanmm2000@gmail.com>
2020-12-06 16:42:05 +0100

    Añade atributos en clase modelo

---
+++ src/main/java/tarea/comida.java
@@ -0,0 +1,19 @@
+package tarea;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aleja
+ */
+public class comida {
+
+    private int Menu;
+    private int Bebida;
+    private int mesa;
+    
+}
